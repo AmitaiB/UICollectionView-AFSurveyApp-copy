2fe8c0cbd7e69048e179baf9f3ba2eebb930b0e4
AmitaiB <blickstein@gmail.com>
2015-10-12 05:04:22 -0400

    Adds custom flow layout file

---
+++ UICollectionView-practice-AFSurveyApp/ABFlowLayout.h
@@ -0,0 +1,15 @@
+//
+//  ABFlowLayout.h
+//  UICollectionView-practice-AFSurveyApp
+//
+//  Created by Amitai Blickstein on 10/12/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define kMaxItemDimension  200.0f
+#define kMaxItemSize       CGSizeMake(kMaxItemDimension, kMaxItemDimension)
+
+@interface ABFlowLayout : UICollectionViewFlowLayout
+@end
---
+++ UICollectionView-practice-AFSurveyApp/ABFlowLayout.m
@@ -0,0 +1,14 @@
+//
+//  ABFlowLayout.m
+//  UICollectionView-practice-AFSurveyApp
+//
+//  Created by Amitai Blickstein on 10/12/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import "ABFlowLayout.h"
+
+
+@implementation ABFlowLayout
+
+@end
